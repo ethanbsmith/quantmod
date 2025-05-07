e188a6854d6824d080eea3767c31512cdde6cf24
Ethan Smith <24379655+ethanbsmith@users.noreply.github.com>
2025-05-07 14:43:49 -0600

    possible universal user agent

--- R/getQuote.R
+++ R/getQuote.R
@@ -35,11 +35,7 @@ function(Symbols,src='yahoo',what, ...) {
     # and the cookies are needed to get the crumb
     curl::handle_setheaders(ses$h, 
                             accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                            "User-Agent" = switch(Sys.info()["sysname"],
-                                                  Linux="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3",
-                                                  Mac="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-                                                  Windows="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183"
-                                                 )
+                            "User-Agent" = "Mozilla/5.0 AppleWebKit/537.3 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3"
                            )
 
     URL <- "https://finance.yahoo.com/"
